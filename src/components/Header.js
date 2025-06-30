f6b7504f793a8abfd2464da8e3ecc9549a561ee0
Juan Alvarez <146954398+JuanAlvarezP@users.noreply.github.com>
2025-06-30 21:58:23 +0000

    Footer incluido y responsive en la app

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,17 +1,19 @@
 import React from "react";
 import "../styles.css";
+import { Link } from "react-router-dom";
 
 export default function Header() {
   return (
     <div className="header">
-      <img
-        className="logo"
-        src="Rick_and_Morty_logo.svg"
-        alt="rickandmortylogo"
-      />
-      <h1 className="app-subtitle">
-        ¡Prueba a buscar un personaje específico para conocer más!
-      </h1>
+      <Link to={"/"}>
+        <img
+          className="logo"
+          src="Rick_and_Morty_logo.svg"
+          alt="rickandmortylogo"
+        />
+      </Link>
+
+      <h1 className="app-subtitle">Lista de personajes</h1>
     </div>
   );
 }

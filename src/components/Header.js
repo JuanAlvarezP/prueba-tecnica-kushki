599e308593f802a09920f84ada1d44fe4158b19b
Juan Alvarez <146954398+JuanAlvarezP@users.noreply.github.com>
2025-07-01 04:31:22 +0000

    Arreglos de detalle de personaje

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,19 +1,21 @@
 import React from "react";
 import "../styles.css";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 
 export default function Header() {
+  const location = useLocation();
+  const isDetailView = location.pathname.startsWith("/character/");
   return (
     <div className="header">
       <Link to={"/"}>
         <img
           className="logo"
-          src="Rick_and_Morty_logo.svg"
+          src="/Rick_and_Morty_logo.svg"
           alt="rickandmortylogo"
         />
       </Link>
 
-      <h1 className="app-subtitle">Lista de personajes</h1>
+      {!isDetailView && <h1 className="app-subtitle">Lista de personajes</h1>}
     </div>
   );
 }
